eda5e9cd021ce85dde4a3d684c2ec5778c481c4a
Rafael Alcaraz Mercado <rafawo1@hotmail.com>
2020-11-10 21:37:42 -0800

    Add copyright/license text

--- mcr/src/layer2guid.rs
+++ mcr/src/layer2guid.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2020 Rafael Alcaraz Mercado. All rights reserved.
+// Licensed under the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>.
+// All files in the project carrying such notice may not be copied, modified, or distributed
+// except according to those terms.
+// THE SOURCE CODE IS AVAILABLE UNDER THE ABOVE CHOSEN LICENSE "AS IS", WITH NO WARRANTIES.
+
 use winapi::shared::{
     bcrypt::{
         BCryptCloseAlgorithmProvider, BCryptCreateHash, BCryptDestroyHash, BCryptFinishHash,
